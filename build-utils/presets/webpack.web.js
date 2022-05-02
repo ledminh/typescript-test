5c13909648aaaab1c0c8a665456d3f924114a060
Minh Le <duyminh.le310@gmail.com>
2022-05-02 12:59:32 -0400

    First commit

---
+++ build-utils/presets/webpack.web.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // support import "./style.css";
+
+module.exports = ({mode}) => {
+    
+    return {
+        output: {
+            filename: 'bundle.js'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: mode == 'production'? [MiniCssExtractPlugin.loader, "css-loader"] :["style-loader", "css-loader"]
+                },
+                {
+                    test: /\.jpe?g$/,
+                    use: [
+                        {
+                            loader: "url-loader",
+                            options: {
+                                limit: 5000
+                            }
+                        }
+                    ]
+                },                
+                
+            ]
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin(),
+            mode == 'production'? new MiniCssExtractPlugin() : undefined
+        ]
+    };
+}
+    
